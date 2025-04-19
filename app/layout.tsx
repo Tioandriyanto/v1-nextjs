c1065f92b102ac7c367da9067690856dfacb8993
Tio Andriyanto <tioandrianto72@gmail.com>
2025-04-19 09:35:57 +0700

    update metadata and title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,47 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
+
+// export const metadata: Metadata = {
+//   title: 'Personal Website',
+//   icons: {
+//     icon: '/favicon.ico',
+//     shortcut: '/favicon.ico',
+//     apple: '/favicon.ico',
+//   },
+//   description: 'Created with Next.js 13.4.4',
+//   keywords: 'Next.js, React, TypeScript',
+//   generator: 'Next.js dev',
+
+// }
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: "Personal Website",
+  description: "Created with Next.js 13.4.4",
+  keywords: "Next.js, React, TypeScript",
+  generator: "Next.js dev",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/favicon.ico",
+    // ikon dari URL, harus ditaruh sebagai array objek
+    other: [
+      {
+        rel: "icon",
+        url: "https://img.icons8.com/ios-filled/50/1A1A1A/t-key.png",
+      },
+    ],
+  },
+};
+//   authors: [ Tio Andriyanto ],
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
